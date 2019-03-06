67e2d07a1de73305cd77786625b788a8d6caadf8
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-06 17:59:21 +0800

    user reviews changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
+
   resources :books do
     resources :reviews
   end
@@ -14,7 +14,25 @@ Rails.application.routes.draw do
   patch '/authors/:id', to: 'authors#update'
   delete '/authors/:id', to: 'authors#destroy'
 
-  resources :users, only: [:show] do
-    resources :reviews
-  end
+
+  # resources :users, only: [:show] do
+  #   resources :reviews
+  # end
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/users/:user_id/reviews', to: 'reviews#index', as: 'user_reviews'
+  post '/users/:user_id/reviews', to: 'reviews#create'
+  get '/users/:user_id/reviews/new', to: 'reviews#new', as: 'new_user_review'
+  get '/users/:user_id/reviews/:id/edit', to: 'reviews#edit', as: 'edit_user_review'
+  get '/users/:user_id/reviews/:id', to: 'reviews#show', as: 'user_review'
+  put '/users/:user_id/reviews/:id', to: 'reviews#update'
+  patch '/users/:user_id/reviews/:id', to: 'reviews#update'
+  delete '/users/:user_id/reviews/:id', to: 'reviews#destroy'
+
+
+
+
+
+
 end
