57961186c1635871d4cf85d08a47f7a4abb01032
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-06 17:49:14 +0800

    routes for authors changed from resources to verb and path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  
   resources :books do
     resources :reviews
   end
-  resources :authors
+  # resources :authors
+  get '/authors', to: 'authors#index', as: 'authors'
+  post '/authors', to: 'authors#create'
+  get '/authors/:id', to: 'authors#show', as: 'author'
+  get '/authors/new', to: 'authors#new', as: 'new_author'
+  get '/authors/:id/edit', to: 'authors#edit', as: 'edit_author'
+  put '/authors/:id', to: 'authors#update'
+  patch '/authors/:id', to: 'authors#update'
+  delete '/authors/:id', to: 'authors#destroy'
+
   resources :users, only: [:show] do
     resources :reviews
   end
