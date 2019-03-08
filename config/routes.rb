609566a7c11fe480953ee4c4593e769ec3b7fe48
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-08 20:29:51 +0800

    fixes the books routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,30 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :books do
-    resources :reviews
-  end
+  # resources :books do
+  #   resources :reviews
+  # end
+
+  get '/books', to: 'books#index', as: 'books'
+  post '/books', to: 'books#create'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  get '/books/:id', to: 'books#show', as: 'book'
+  put '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
+  # patch '/books/:id', to: 'books#update'
+
+  get '/books/:book_id/reviews', to: 'reviews#index', as: 'book_reviews'
+  post '/books/:book_id/reviews', to: 'reviews#create'
+  get '/books/:book_id/reviews/new', to: 'reviews#new', as: 'new_book_review'
+  get '/books/:book_id/reviews/:id/edit', to: 'reviews#edit', as: 'edit_book_review'
+  get '/books/:book_id/reviews/:id', to: 'reviews#show', as: 'book_review'
+  put '/books/:book_id/reviews/:id', to: 'reviews#update'
+  patch '/books/:book_id/reviews/:id', to: 'reviews#update'
+  delete '/books/:book_id/reviews/:id', to: 'reviews#destroy'
+
+
+
   # resources :authors
   get '/authors', to: 'authors#index', as: 'authors'
   post '/authors', to: 'authors#create'
